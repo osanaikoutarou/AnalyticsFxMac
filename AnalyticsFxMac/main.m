c8e415243b453bf46112848df37bf05f30d4b5bd
osanai <osanai@cr-040.local>
2017-08-29 16:14:38 +0900

    Initial Commit

---
+++ AnalyticsFxMac/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AnalyticsFxMac
+//
+//  Created by osanai on 2017/08/29.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
